623373f0c86d6f8f81eb23b07427d433fe7681ce
vikram(wsl) <vikramjit360@gmail.com>
2024-11-07 21:59:26 +0530

    11.9 - test(e2e): add e2e test to ensure we can navigate to a certain pokémon's page

--- e2e/pokedex.spec.js
+++ e2e/pokedex.spec.js
@@ -5,5 +5,11 @@ describe('Pokedex', () => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+  test('we can navigate to a certain pokémon\'s page', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('charizard').click()
+    await expect(page.getByText('blaze')).toBeVisible()
+    await expect(page.getByText('solar power')).toBeVisible()
   })
 })
